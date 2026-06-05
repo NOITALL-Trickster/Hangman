b3ee4d3eff04c5bbeb4c361104d1db7f201cf1db
NOITALL-Trickster <ashikashokan3303@gmail.com>
2026-06-05 03:47:16 -0400

    saved the user input for a guess word and made it lowercase

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ rand = random.randint(0, len(word_list)-1)
 chosen_word = word_list[rand]
 
 print(chosen_word)
+# TODO: Ask the user to guess a letter and assign their answer to a variable. Make it lower case
+guess = input("Guess a letter: ")
+guess = guess.lower()
