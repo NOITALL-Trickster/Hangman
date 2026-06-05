86a8bd8da4e29b9f59825cdb9a8bcc13bb389cb5
NOITALL-Trickster <ashikashokan3303@gmail.com>
2026-06-05 04:01:11 -0400

    replaced the placeholders with guessed right words

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
-word_list = ['lamp', 'fire', 'king']
-# TODO: randomly get a word from word_lis and tprint the chosen world
+word_list = ['lamp', 'fire', 'king','wakanda']
+
 rand = random.randint(0, len(word_list)-1)
 chosen_word = word_list[rand]
 
@@ -13,9 +13,10 @@ print()
 
 guess = input("Guess a letter: ")
 guess = guess.lower()
-# TODO: Check if the letter user guessed is actually one of the right letters in chosen_word
+# TODO: Replace the guess letter with right places in placeholder
 for letter in chosen_word:
     if letter == guess:
-        print("Right")
+        print(letter, end='')
     else:
-        print("Wrong")
+        print('_',end='')
+print()
