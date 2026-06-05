edb587c829895e3e69933c259158358d4881ea02
NOITALL-Trickster <ashikashokan3303@gmail.com>
2026-06-05 03:44:20 -0400

    Randomly chosen a word from word_list and printed it out

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import random
 
+word_list = ['lamp', 'fire', 'king']
+# TODO: randomly get a word from word_lis and tprint the chosen world
+rand = random.randint(0, len(word_list)-1)
+chosen_word = word_list[rand]
+
+print(chosen_word)
