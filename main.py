e2256e25f542f7a16c86876ed447d77d2cf4a946
NOITALL-Trickster <ashikashokan3303@gmail.com>
2026-06-05 03:50:55 -0400

    check if the user letter is in chosen_word

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ print(chosen_word)
 # TODO: Ask the user to guess a letter and assign their answer to a variable. Make it lower case
 guess = input("Guess a letter: ")
 guess = guess.lower()
+# TODO: Check if the letter user guessed is actually one of the right letters in chosen_word
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
