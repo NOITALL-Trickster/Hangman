f4736e28acf78ffd9ebdfcc4356599cf45be56ac
NOITALL-Trickster <ashikashokan3303@gmail.com>
2026-06-05 04:14:09 -0400

    a while loop for user to guess again and used a display variable

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ rand = random.randint(0, len(word_list)-1)
 chosen_word = word_list[rand]
 
 print(chosen_word)
-# TODO: Create a placeholder for the chosen_word
+
 for i in range(len(chosen_word)):
     print(end='_')
 print()
-
-guess = input("Guess a letter: ")
-guess = guess.lower()
-# TODO: Replace the guess letter with right places in placeholder
-for letter in chosen_word:
-    if letter == guess:
-        print(letter, end='')
-    else:
-        print('_',end='')
-print()
+# TODO: Create a while loop for the user to guess again
+display = ""
+user_won = False
+while not user_won:
+    guess = input("Guess a letter: ")
+    guess = guess.lower()
+    # TODO: Replace the guess letter with right places in placeholder
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+        else:
+            display += "_"
+    print(display)
